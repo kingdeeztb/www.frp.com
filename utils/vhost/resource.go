ccb55ad519e8cdfbc17410aeb6f5abc2cbb06969
kingdeeztb <kingdeeztb@163.com>
2020-05-20 16:36:40 +0800

    404html
    404html

--- utils/vhost/resource.go
+++ utils/vhost/resource.go
@@ -30,22 +30,34 @@ var (
 const (
 	NotFound = `<!DOCTYPE html>
 <html>
+
 <head>
-<title>Not Found</title>
-<style>
-    body {
-        width: 35em;
-        margin: 0 auto;
-        font-family: Tahoma, Verdana, Arial, sans-serif;
-    }
-</style>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>404 找不到此页面/(ㄒoㄒ)/~~</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+<style>*{padding:0;margin:0}a{text-decoration:none}.notfoud-container .img-404{height:155px;background:url(https://www.ituku.me/images/0c87.png) center center no-repeat;-webkit-background-size:150px auto;margin-top:40px;margin-bottom:20px}.notfoud-container .notfound-p{line-height:22px;font-size:17px;padding-bottom:15px;border-bottom:1px solid #f6f6f6;text-align:center;color:#262b31}.notfoud-container .notfound-reason{color:#9ca4ac;font-size:13px;line-height:13px;text-align:left;width:210px;margin:0 auto}.notfoud-container .notfound-reason p{margin-top:13px}.notfoud-container .notfound-reason ul li{margin-top:10px;margin-left:36px}.notfoud-container .notfound-btn-container{margin:40px auto 0;text-align:center}.notfoud-container .notfound-btn-container .notfound-btn{display:inline-block;border:1px solid #ebedef;background-color:#239bf0;color:#fff;font-size:15px;border-radius:5px;text-align:center;padding:10px;line-height:16px;white-space:nowrap}</style>
 </head>
 <body>
-<h1>The page you requested was not found.</h1>
-<p>Sorry, the page you are looking for is currently unavailable.<br/>
-Please try again later.</p>
-<p>The server is powered by <a href="https://github.com/fatedier/frp">frp</a>.</p>
-<p><em>Faithfully yours, frp.</em></p>
+
+    <div class="notfoud-container">
+        <div class="img-404">
+        </div>
+        <p class="notfound-p">哎呀迷路了...</p>
+        <div class="notfound-reason">
+            <p>可能的原因：</p>
+            <ul>
+                <li>网站出去溜达了</li>
+                <li>域名不正确</li>
+                <li>内网端不存在该页面</li>
+                <li>我们的服务器被外星人劫持了</li>
+		<li>赶紧通知Pomelogoo#gmail.com</li>
+            </ul>
+        </div>
+        
+    </div>
+
 </body>
 </html>
 `
